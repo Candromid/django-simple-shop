674cae7de165ace4806f4224e39b5723ba501399
candromid <mainaccnurbek@mail.ru>
2026-01-07 01:22:33 +0500

    feat: added model class Category

--- shop/main/models.py
+++ shop/main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):   
+    name = models.CharField(max_length=100, db_index=True)      
+    
+    #  slag  - конвертация name в url путь для фильтрации
+    slug = models.CharField(max_length=100, unique=True)   
+    
+    
+    # внутренний класс в модели, для метаданных и настройки поведения модели
+    class Meta:    
+        # сортировка - упорядочивание записей ( по умолчанию - по дате создания )
+        ordering = ('name',) 
+                
+        # человекочитаемые имена для админки Django
+        verbose_name = 'Категория' 
+        verbose_name_plural = "Категории"
+
+    # метод отображения объекта в админке (в данном случае по имени name)
+    def __str__(self):
+        return self.name
