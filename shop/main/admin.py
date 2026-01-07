c6e70e054d7042a05640e8388903c5affacaa21a
candromid <mainaccnurbek@mail.ru>
2026-01-07 16:11:49 +0500

    feat: added Product models and registration models on admin.py

--- shop/main/admin.py
+++ shop/main/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+# декаратор регистрации 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # указываем какие поля будут отображаться в админке
+    list_display = ['name', 'slug'] 
+    
+    # автозаполнение поля 'slug' на основе значения другого поля 'name' в админке
+    prepopulated_fields = {'slug': ('name',)}
+    
+    
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'available', 
+                    'created', 'updated']
+    list_filter = ['available', 'created', 'updated', 'category']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+    
--- shop/main/models.py
+++ shop/main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Category(models.Model):   
     name = models.CharField(max_length=100, db_index=True)      
     
-    #  slag  - конвертация name в url путь для фильтрации
+    #  slug  - конвертация name в url путь для фильтрации
     slug = models.CharField(max_length=100, unique=True)   
     
     
@@ -19,3 +19,34 @@ class Category(models.Model):
     # метод отображения объекта в админке (в данном случае по имени name)
     def __str__(self):
         return self.name
+    
+    
+class Product(models.Model):
+    # наследуем класс Category, чтобы добавленные продукты 
+    # были сгруппированы в соответствующуюся категорию
+    # ForeignKey = наследование от класса Category
+    # related_name = явное имя для обращения к полю
+    # CASCADE = позволяет удалить все записи относящиеся к родительскому классу
+    category = models.ForeignKey(Category, related_name="products", 
+                                 on_delete=models.CASCADE)
+    
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.CharField(max_length=100, unique=100)
+    
+    # upload_to - путь хранения фотографии вместе с указанием даты
+    # blank  = True - позволяет полю быть пустым ( null = true ) 
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    
+    # decimal_places = количество чисел после запятой
+    price = models.DecimalField(max_digits=10, decimal_places=2)  
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)  
+    
+    class Meta:
+        ordering = ('name', )
+        
+    def __str__(self):
+        return self.name
+    
